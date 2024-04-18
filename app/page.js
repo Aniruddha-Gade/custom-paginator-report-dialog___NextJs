5b2d5145810e0d2fde1723ae4902b17be4f1a9f6
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-18 12:31:05 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className='bg-red-500 p-10 text-5xl underline h-screen'>
+
+      <p>
+        Hello World
+      </p>
+
+    </main>
+  );
+}
