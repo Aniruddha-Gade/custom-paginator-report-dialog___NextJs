de59eb91ff257a6bfb1a82a7f595f00b2d8c0b1e
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-18 22:51:20 +0530

    add: reports page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='bg-red-500 p-10 text-5xl underline h-screen'>
+    <main className='bg-[#101418] flex-center p-10 text-5xl  h-screen'>
 
-      <p>
-        Hello World
-      </p>
+      <button className="font-bold bg-slate-600 hover:bg-slate-700 p-4 rounded-2xl duration-300">
+        <Link href='/reports'>
+          Go to Reports Page 📝
+        </Link>
+      </button>
 
     </main>
   );
