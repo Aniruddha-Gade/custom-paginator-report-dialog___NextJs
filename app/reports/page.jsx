2db4dc6f45bc872cc47920ce539f54a9cd99f10c
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-19 14:21:14 +0530

    add: DialogTrigger and DialogContent component

--- app/reports/page.jsx
+++ app/reports/page.jsx
@@ -1,7 +1,12 @@
-import { Dialog } from '@/components/Dialog'
-import React from 'react'
+'use client'
+
+import { Dialog, DialogContent, DialogTrigger } from '@/components/Dialog'
+import React, { useState } from 'react'
 
 const page = () => {
+
+  const [showDialog, setShowDialog] = useState(false)
+
   return (
     <div className='wrapper bg-[#101418] h-screen'>
       <h1 className='text-4xl font-bold text-green-500 '>
@@ -10,7 +15,23 @@ const page = () => {
 
 
       <Dialog>
-        This a Dialog component
+        <DialogTrigger>
+          <button
+            onClick={() => setShowDialog(prev => !prev)}
+            className='text-lg font-semibold bg-green-950 hover:bg-green-700/35 duration-200 text-green-400 py-2 px-4 rounded-xl '>
+            Open Reports
+          </button>
+        </DialogTrigger>
+
+
+        <DialogContent
+          showDialog={showDialog}
+          className={''}
+        >
+          This is content of dialog
+
+        </DialogContent>
+
       </Dialog>
 
 
--- components/Dialog.jsx
+++ components/Dialog.jsx
@@ -1,10 +1,34 @@
+'use client'
+
+
 import React from 'react';
 
 // Dialog Component
-export const Dialog = ({ children, isOpen, onClose, title, description, actions }) => {
-    return(
+export const Dialog = ({ children }) => {
+    return (
         <div>
             {children}
         </div>
     )
 };
+
+export const DialogTrigger = ({ children, className }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+};
+
+
+export const DialogContent = ({ children, className, showDialog }) => {
+    // console.log('showDialog = ', showDialog)
+    if (!showDialog) return null;
+
+    return (
+        <div className={`fixed inset-7 rounded-2xl text-black bg-white flex items-center justify-center ${className}`}>
+            {children}
+        </div>
+    )
+};
+
