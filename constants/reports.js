b8a76a68abea83d8c3ff66bba7106309a3a7003f
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-20 20:13:03 +0530

    add: more data for reports

--- constants/reports.js
+++ constants/reports.js
@@ -1,17 +1,38 @@
 
 // reports data
-export const reports = [
+export const reportsData = [
     {
+        id: 0,
         date: '22.07.2021',
         reportName: 'APP_URL_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv',
         time: '16:02 PM',
     },
     {
+        id: 1,
         date: '20.07.2021',
         reportName: 'RUR_L1_01_01_2021_31_03_2021.csv',
         time: '17:00 PM',
     },
     {
+        id: 2,
+        date: '19.07.2021',
+        reportName: 'RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv',
+        time: '15:00 PM',
+    },
+    {
+        id: 3,
+        date: '22.07.2021',
+        reportName: 'APP_URL_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv',
+        time: '16:02 PM',
+    },
+    {
+        id: 4,
+        date: '20.07.2021',
+        reportName: 'RUR_L1_01_01_2021_31_03_2021.csv',
+        time: '17:00 PM',
+    },
+    {
+        id: 5,
         date: '19.07.2021',
         reportName: 'RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv',
         time: '15:00 PM',
