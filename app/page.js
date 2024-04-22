9a37c92f6f3557f5d126cfbc1f7d237fb691f45a
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-22 21:00:14 +0530

    update: 'go to reports' button

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='bg-[#101418] flex-center p-10 text-5xl  h-screen'>
+    <main className='bg-[#101418] flex-center p-10 h-screen'>
 
-      <button className="font-bold bg-slate-600 hover:bg-slate-700 p-4 rounded-2xl duration-300">
-        <Link href='/reports'>
-          Go to Reports Page 📝
-        </Link>
-      </button>
+      <Link
+        href='/reports'
+        className="font-bold text-lg sm:text-5xl bg-slate-600 hover:bg-slate-700 p-4 rounded-2xl duration-300"
+      >
+        Go to Reports Page 📝
+      </Link>
 
     </main>
   );
